151a09e89d630ba792e1b2db86501fc90ff900ed
Clement <clementsreeves@gmail.com>
2017-12-02 09:15:45 +0000

    docstring and shebang

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import utils
 
 def solve_captcha(s, shift=1):
--- day2.py
+++ day2.py
@@ -1,15 +1,37 @@
+#!/usr/bin/env python3
+
 import utils
 import itertools
+
+class InvalidInputError(Exception):
+    pass
 
 def checksum(matrix):
     return sum([(max(row) - min(row)) for row in matrix])
 
 def even_divisor(l):
+    """Finds the quotient of the two numbers where one divides the other.
+
+    Assumes that there are only two numbers where one divides the other. Finds
+    those numbers, and returns their quotient.
+
+    Args:
+        l: A list of integers
+
+    Returns:
+        An integer quotient of the two numbers found
+
+    Raises:
+        InvalidInputError: The list did not contain two numbers where one
+                           evenly divides the other
+    """
     for combo in itertools.combinations(l, 2):
         low, high = sorted(combo)
         q, r = divmod(high, low)
         if r == 0: return q
-        
+    raise InvalidInputError("The list did not contain two numbers where one"
+                              " evenly divides the other")    
+
 def divsum(matrix):
     return sum([even_divisor(row) for row in matrix])
 
